4cfce1ce4d5634ab0f013bc081f35fc51590db34
tornado4444 <tornadokiller4444@gmail.com>
2026-08-10 22:40:28 +0300

    Starting release memory.h

--- include/mips64/memory.h
+++ include/mips64/memory.h
@@ -1,9 +1,17 @@
 #pragma once
 
+#include "mips64/core.h"
+
 #define MAX_SIZE 1024u
 #define BLOCK_SIZE 16u
 
+/* TODO FOR MEMORY
+	1) LoadMemory, StoreMemory();
+	2) Little-Endian, Big-Endian;(BigEndianMem)
+	3) memory_size = 64 * 1024 * 1024
+*/
+
 // FOR MEMORY ~1 MB
 typedef struct Memory {
-
+	uint8_t memory[MAX_SIZE];
 } Memory;
